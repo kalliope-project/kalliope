5273fb51e33b35cf7a7a9fd33a6cef17095b8c41
nico <nico.marcq@gmail.com>
2016-09-14 00:14:45 +0200

    add todo

--- core/Neurone.py
+++ core/Neurone.py
@@ -2,12 +2,13 @@ import importlib
 
 
 class Neurone:
-    def __init__(self):
-        # Here we load the stt and tts from settings
+    def __init__(self, stt=None, tts=None):
+        # TODO load the stt and tts from settings
         self.stt = "snowboy"
         self.tts = "pico2wave"
 
     def say(self, message):
         # here we use the tts to make jarvis talk
+        # the module is imported on fly, depending on the selected tts from settings
         tts_backend = importlib.import_module("tts." + self.tts)
         tts_backend.say(message)
