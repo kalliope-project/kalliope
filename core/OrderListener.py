379e501878f7fbc9b3b49c038c96af93389479ea
nico <nico.marcq@gmail.com>
2016-09-17 12:10:23 +0200

    google stt test FR ok

--- core/OrderListener.py
+++ core/OrderListener.py
@@ -21,6 +21,8 @@ class OrderListener:
         # obtain audio from the microphone
         r = sr.Recognizer()
         with sr.Microphone() as source:
+            # listen for 1 second to calibrate the energy threshold for ambient noise levels
+            r.adjust_for_ambient_noise(source)
             print("Say something!")
             audio = r.listen(source)
 
@@ -29,7 +31,7 @@ class OrderListener:
             # for testing purposes, we're just using the default API key
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio, key=None, language="fr-FR"))
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
