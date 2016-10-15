d8fcb775caec3d643952ac5b068e1a6924d7e667
nico <nico.marcq@gmail.com>
2016-10-15 13:00:32 +0200

    update system date neuron

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import re
 from collections import Counter
 
 from core import OrderAnalyser
+from neurons import Systemdate
 from neurons.tasker_autoremote.tasker_autoremote import Tasker_autoremote
 
 logging.basicConfig()
@@ -11,21 +12,12 @@ logger = logging.getLogger("kalliope")
 logger.setLevel(logging.DEBUG)
 
 
-# # This does not work because of different encoding when using accent
-# from core import OrderAnalyser
-# # order = "kalliope régle le réveil pour sept heures et vingt minutes"
-# # order = "mais nous de la musique"
-#
-# order = "arrête la musique"
-# # order = order.decode('utf-8')
-# # print type(order)
-# oa = OrderAnalyser(order)
-#
-# oa.start()
-key = "mykey"
-message = "lost my phone"
 
-tk = Tasker_autoremote(key=key, message=message)
+order = "test heure"
+oa = OrderAnalyser(order)
+oa.start()
+
+
 
 
 
