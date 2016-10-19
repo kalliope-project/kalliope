8f857db71c396a29a87dd58c0833db6f43c6286a
nico <nico.marcq@gmail.com>
2016-10-19 19:03:15 +0200

    get synapse by name

--- core/RestAPI/FlaskAPI.py
+++ core/RestAPI/FlaskAPI.py
@@ -12,7 +12,10 @@ class FlaskAPI(threading.Thread):
         self.app = app
         self.brain = brain
 
+        self.brain_yaml = BrainLoader.get_yaml_config()
+
         self.app.add_url_rule('/synapses', view_func=self.get_synapses, methods=['GET'])
+        self.app.add_url_rule('/synapses/<synapse_name>', view_func=self.get_synapse, methods=['GET'])
 
     def run(self):
         self.app.run(host='0.0.0.0', debug=True, threaded=True, use_reloader=False)
@@ -22,5 +25,25 @@ class FlaskAPI(threading.Thread):
         """
         get all synapse
         """
-        data = jsonify(synapses=BrainLoader.get_yaml_config())
+        data = jsonify(synapses=self.brain_yaml)
         return data, 200
+
+    @requires_auth
+    def get_synapse(self, synapse_name):
+        """
+        get all synapse
+        """
+        synapse_target = None
+        all_synapse = self.brain_yaml
+        for el in all_synapse:
+            print el
+            if el[0]["name"] in synapse_name:
+                synapse_target = el[0]
+        if synapse_target is not None:
+            data = jsonify(synapses=synapse_target)
+            return data, 200
+
+        data = {
+            "synapse name not found": "%s" % synapse_name
+        }
+        return jsonify(error=data), 404
