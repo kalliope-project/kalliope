60247058de2cdf212bd6f8f31b79ec4ed0aaf9cd
nico <nico.marcq@gmail.com>
2016-10-19 19:38:27 +0200

    implement api in the main controller

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import re
 from collections import Counter
 
 from flask import Flask
-
+from core.RestAPI.FlaskAPI import FlaskAPI
 from core import OrderAnalyser
 from core.ConfigurationManager import SettingLoader
 from core.ConfigurationManager import YAMLLoader
 from core.ConfigurationManager.BrainLoader import BrainLoader
-from core.RestAPI.FlaskAPI import FlaskAPI
+
 from neurons import Systemdate
 from neurons.tasker_autoremote.tasker_autoremote import Tasker_autoremote
 
