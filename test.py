66945c6b2950124236ee4a577dc685dc7cd6e8a0
nico <nico.marcq@gmail.com>
2016-10-29 18:17:52 +0200

    add wake on lan to list of neuron

--- test.py
+++ test.py
@@ -10,24 +10,8 @@ logger.setLevel(logging.DEBUG)
 
 
 brain = BrainLoader.get_brain()
-
-brain2 = BrainLoader.get_brain()
-brain3 = BrainLoader.get_brain()
-brain4 = BrainLoader.get_brain()
-
-
-print brain is brain2
-print brain is brain3
-print brain is brain4
-print brain4 is brain2
-
-set = SettingLoader.get_settings()
-set2 = SettingLoader.get_settings()
-set3 = SettingLoader.get_settings()
-set4 = SettingLoader.get_settings()
-
-print set is set2
-print set is set3
-print set is set4
-print set3 is set2
+print brain.brain_yaml
+for synapse in brain.synapses:
+    print "test"
+    print synapse.name
 
