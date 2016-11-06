418b9a521f44888214acbb5f83cc620c4995e9bd
nico <nico.marcq@gmail.com>
2016-11-06 14:41:00 +0100

    add other pre compiled libs to help the installation on RPi

--- install/install_kalliope.sh
+++ install/install_kalliope.sh
@@ -4,6 +4,7 @@
 sudo apt-get install -y python-pip libssl-dev
 
 # this is used to help the RPI
+sudo apt-get install -y libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
 sudo apt-get install -y libffi-dev python-yaml python-pycparser python-paramiko python-markupsafe
 
 # install ansible
