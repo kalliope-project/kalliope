34ad8f103313bcba8afdadbc97d4dc3234953ac2
Nicolas Marcq <nico.marcq@gmail.com>
2016-11-06 13:42:34 +0100

    update install
    Those packages help the RPI to install ansible without having to build some libs

--- install/install_kalliope.sh
+++ install/install_kalliope.sh
@@ -3,6 +3,9 @@
 # install packages
 sudo apt-get install -y python-pip libssl-dev
 
+# this is used to help the RPI
+sudo apt-get install -y libffi-dev python-yaml python-pycparser python-paramiko python-markupsafe
+
 # install ansible
 sudo pip install ansible==2.1.1.0
 
