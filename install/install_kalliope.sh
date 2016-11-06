86da42d424bc4ab7cb59ee3f15715975ad94729b
Nicolas Marcq <nico.marcq@gmail.com>
2016-11-07 00:52:18 +0100

    update install
    last Raspbian Jessie does not handle https package without this package

--- install/install_kalliope.sh
+++ install/install_kalliope.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y python-pip libssl-dev
 
 # this is used to help the RPI
 sudo apt-get install -y libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
-sudo apt-get install -y libffi-dev python-yaml python-pycparser python-paramiko python-markupsafe
+sudo apt-get install -y libffi-dev python-yaml python-pycparser python-paramiko python-markupsafe apt-transport-https
 
 # install ansible
 sudo pip install ansible==2.1.1.0
