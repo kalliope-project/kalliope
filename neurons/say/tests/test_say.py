87942dffe899c0b25e239d45ac36cfd63e40302a
monf <monf@gmail.com>
2016-11-25 14:36:23 +0100

    [Tests] Add Say neuron param tests

---
+++ neurons/say/tests/test_say.py
@@ -0,0 +1,27 @@
+import unittest
+
+from core.NeuronModule import MissingParameterException
+from neurons.say.say import Say
+
+
+class TestSay(unittest.TestCase):
+
+    def setUp(self):
+        self.message="message"
+        self.random = "random"
+
+    def testParameters(self):
+        def run_test(parameters_to_test):
+            with self.assertRaises(MissingParameterException):
+                Say(**parameters_to_test)
+
+        # empty
+        parameters = dict()
+        run_test(parameters)
+
+        # missing message
+        parameters = {
+            "random": self.random
+        }
+        run_test(parameters)
+
