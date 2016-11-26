68845a30356ddc81da87ff43690aa37d90c371ee
nico <nico.marcq@gmail.com>
2016-11-26 18:07:47 +0100

    update script neuron, can now be used with async

--- neurons/script/script.py
+++ neurons/script/script.py
@@ -1,18 +1,57 @@
 import subprocess
 import os
+import threading
 
 from core.NeuronModule import NeuronModule, MissingParameterException, InvalidParameterException
+
+
+class AsyncShell(threading.Thread):
+    """
+    Class used to run an asynchronous Shell command
+
+    .. notes:: Impossible to get the success code of the command
+    """
+    def __init__(self, path):
+        self.stdout = None
+        self.stderr = None
+        self.path = path
+        threading.Thread.__init__(self)
+
+    def run(self):
+        p = subprocess.Popen(self.path,
+                             shell=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+
+        self.stdout, self.stderr = p.communicate()
 
 
 class Script(NeuronModule):
     def __init__(self, **kwargs):
         super(Script, self).__init__(**kwargs)
         self.path = kwargs.get("path", None)
+        # get if the user select a blocking command or not
+        self.async = kwargs.get('async', False)
 
         # check parameters
         if self._is_parameters_ok():
-            p = subprocess.Popen(self.path, stdout=subprocess.PIPE, shell=True)
-            (output, err) = p.communicate()
+            # run the command
+            if not self.async:
+                p = subprocess.Popen(self.path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                (output, err) = p.communicate()
+                self.output = output
+                self.returncode = p.returncode
+                print self.output
+                print self.returncode
+                message = {
+                    "output": self.output,
+                    "returncode": self.returncode
+                }
+                self.say(message)
+
+            else:
+                async_shell = AsyncShell(path=self.path)
+                async_shell.start()
 
     def _is_parameters_ok(self):
         """
