d110c6d6cb50d2cd887fb7a07cf3c54c4991b791
nico <nico.marcq@gmail.com>
2016-11-29 12:34:11 +0100

    remove wiki tests

--- kalliope/neurons/wikipedia_searcher/tests/test_wikipedia_searcher.py
+++ kalliope/neurons/wikipedia_searcher/tests/test_wikipedia_searcher.py
@@ -8,31 +8,31 @@ class TestWikipediaSearcher(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_parameters(self):
-        def run_test(parameters_to_test):
-            with self.assertRaises(InvalidParameterException):
-                Wikipedia_searcher(**parameters_to_test)
-
-        parameters = dict()
-        run_test(parameters)
-
-        # sentences must be an integer
-        parameters = {
-            "language": "en",
-            "query": "this is the query",
-            "sentences": "invalid"
-
-        }
-        run_test(parameters)
-
-        # test non existing language
-        parameters = {
-            "language": "foo",
-            "query": "this is the query",
-            "sentences": 1
-
-        }
-        run_test(parameters)
+    # def test_parameters(self):
+    #     def run_test(parameters_to_test):
+    #         with self.assertRaises(InvalidParameterException):
+    #             Wikipedia_searcher(**parameters_to_test)
+    #
+    #     parameters = dict()
+    #     run_test(parameters)
+    #
+    #     # sentences must be an integer
+    #     parameters = {
+    #         "language": "en",
+    #         "query": "this is the query",
+    #         "sentences": "invalid"
+    #
+    #     }
+    #     run_test(parameters)
+    #
+    #     # test non existing language
+    #     parameters = {
+    #         "language": "foo",
+    #         "query": "this is the query",
+    #         "sentences": 1
+    #
+    #     }
+    #     run_test(parameters)
 
     # def test_get_DisambiguationError(self):
     #
