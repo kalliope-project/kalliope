a4936f9ed41981ce92a13787e35c53f5b86dcdb8
nico <nico.marcq@gmail.com>
2018-05-06 01:30:28 -0700

    remove shell GUI

--- kalliope/core/__init__.py
+++ kalliope/core/__init__.py
@@ -1,6 +1,5 @@
 from kalliope.core.OrderAnalyser import OrderAnalyser
 from kalliope.core.OrderListener import OrderListener
-from kalliope.core.ShellGui import ShellGui
 from kalliope.core.Utils.Utils import Utils
 from kalliope.core.Utils import FileManager
 from kalliope.core.ResourcesManager import ResourcesManager
